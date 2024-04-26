bddad90cc3f03e4173a28c3f0cb07bf1e8efbeea
Anineco Nyanta <anineco@nifty.com>
2024-04-27 06:56:33 +0900

    prettify

--- src/centercross.js
+++ src/centercross.js
@@ -5,11 +5,9 @@ export default class CenterCross extends Control {
   constructor(options = {}) {
     const element = options.element;
     super({ element });
-    const img = new Image();
-    // img.src = 'crosshair.gif';
+    const img = new Image(24, 24);
+    // img.src = './crosshair.gif';
     img.src = 'data:image/gif;base64,R0lGODlhGAAYAPAAAICAgAAAACH5BAEAAAEAIf8LSW1hZ2VNYWdpY2sOZ2FtbWE9MC40NTQ1NDUALAAAAAAYABgAAAIxjI+puwAMk4s0zAovbm9z/4FSJ5Ym4qSpcakq67ZdrJ02WJe5uOOk2fMEN0NM8XYoAAA7';
-    img.width = 24;
-    img.height = 24;
     element.appendChild(img);
     this.container = element;
     this.container.className = options.className || 'ol-centercross ol-unselectable';
